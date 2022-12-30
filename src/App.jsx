14f11063811ed4dc2863222bd30e6d7ef65aaeff
Miguel Chavez <toonchavez8@gmail.com>
2022-12-30 08:22:47 -0600

    add new imgs for card carusel | create api folder with data js | modify app js to add max style for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from './api/data'
 
 /*
 Challenge: Build the Navbar component.
@@ -8,11 +10,24 @@ Check the Figma file for the design specifics.
 */
 
 export default function App() {
+	
+	const cards = data.map (card => {
+		return (
+			<Card 
+			key={card.id}
+			card={card}
+	/>
+		)
+	})
+
 	return (
 		<>
-			<Navbar />
 		<main>
+			<Navbar />
 			<Hero />
+			<section className="cards">
+				{cards}
+			</section>
 		</main>
 		</>
 	);
---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../css/Card.css'
+
+
+export default function Card(prop) {
+  console.log(prop)
+
+  let badgetext
+  if (prop.card.openSpots === 0) {
+    badgetext = 'Sold out'
+  } else if (prop.card.location === "Online") {
+    badgetext = "ONLINE"
+  }
+
+  return (
+    
+    <div className='card'>
+     {badgetext && <div className='card--badge'>{badgetext}</div>}
+      <img className="card--img" src={`../src/assets/${prop.card.coverImg}`} alt="Katie Zaferes"/>
+      <div className="card--stats">
+        <img className='card--star' src="../src/assets/Star 1.png" />
+        <span>{prop.card.stats.rating}</span>
+        <span className='gray'>({prop.card.stats.reviewCount}) • </span>
+        <span className='gray'>{prop.card.location}</span>
+      </div>
+      <p>{prop.card.title}</p>
+      <p className='card--price'><span className="bold">From ${prop.card.price}</span> / person</p>
+    </div>
+  )
+}
